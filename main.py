70ae3fb092bec1fac30406bfb20b6fad361588b3
Jacson <k.jacson@gmail.com>
2024-03-21 14:11:03 -0300

    Teste do Meu Primeiro Commit via Git do arquivo main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+print("jornada de dados")
+
+#Variáveis
+idade = 34
+nome = "Luciano"
+
+#Print
+print(idade)
+
+#soma
+nova_idade= idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
